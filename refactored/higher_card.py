96188bad16593dd4d7253c67a7d96dfe00a06ddf
Richard Ng <richardcrng@gmail.com>
2021-01-18 07:31:23 +0000

    Improve deck representation

--- refactored/higher_card.py
+++ refactored/higher_card.py
@@ -13,8 +13,8 @@ def play(bet):
     if bet <= 0: return utils.handle_zero_bet()
     
     utils.print_with_divider("Let's play a game of cards!", before=True, after=False)
-    player_card = pick_random_card()
-    computer_card = pick_random_card()
+    # Using destructuring to get the first two elements of a list
+    player_card, computer_card, *_ = generate_shuffled_deck()
     print_selected_cards(player_card, computer_card)
     outcome = find_outcome(player_card, computer_card)
     return utils.find_and_print_winnings(bet, outcome)
@@ -33,12 +33,24 @@ def find_outcome(player_value, computer_value):
     else:
         return utils.GAME_LOSS
 
+def generate_deck_values():
+    deck = []
+    # Populate deck with values of four suits
+    for n in range(4):
+        deck += generate_suit_values()
+    return deck
+
+def generate_shuffled_deck():
+    deck = generate_deck_values()
+    random.shuffle(deck) # shuffles in-place (mutates)
+    return deck
+
+def generate_suit_values():
+    # Use 1 to 13 to represent Ace to King
+    return list(range(1, 13))
+
 def print_selected_cards(player_card, computer_card):
     """Print the player and computer cards.
     """
     print(f"Your card was {player_card}")
     print(f"Their card was {computer_card}")
-
-def pick_random_card():
-    # Use 1 to 13 to represent Ace to King
-    return random.randint(1, 13)
